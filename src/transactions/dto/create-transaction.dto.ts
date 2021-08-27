711ef4d26a2b347f23bea1b6bce334fb0822ae0e
Stanisław Łagodzic <stanislaw.lagodzic@iterative.pl>
2021-08-27 16:50:18 +0200

    Create create transaction DTO

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,46 @@
-export class CreateTransactionDto {}
+import {
+  IsDecimal,
+  IsISO8601,
+  IsPositive,
+  IsString,
+  IsUUID,
+  ValidateIf,
+} from 'class-validator';
+import { Account } from 'src/accounts/entities/account.entity';
+
+export class CreateTransactionDto {
+  @IsUUID('4')
+  account: Account;
+
+  @IsString()
+  // @IsISO8601()
+  transactionDate: string;
+
+  @IsString()
+  transactionDesc: string;
+
+  @IsString()
+  transactionType: string;
+
+  // @Optional()
+  @ValidateIf((o) => o.creditAmount === undefined)
+  @IsDecimal({
+    force_decimal: true,
+    decimal_digits: '2',
+  })
+  debitAmount: number;
+
+  // @Optional()
+  @ValidateIf((o) => o.debitAmount === undefined)
+  @IsDecimal({
+    force_decimal: true,
+    decimal_digits: '2',
+  })
+  creditAmount: number;
+
+  @IsDecimal({
+    force_decimal: true,
+    decimal_digits: '2',
+  })
+  balance: number;
+}
