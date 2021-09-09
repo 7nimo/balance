ad5edc6ce68cb0b5f5170ce7ff697032c276b8b9
Stanisław Łagodzic <stanislaw.lagodzic@iterative.pl>
2021-09-10 00:32:26 +0200

    add csv parser factory

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -5,11 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionEntity } from './entities/transaction.entity';
 import { StatementSavedListener } from 'src/transaction/listeners/statement-saved.listener';
 import { AccountsModule } from 'src/account/account.module';
+import { CsvParserFactory } from 'src/common/factories/csv-parser.factory';
 import { CsvParserService } from 'src/common/services/csv-parser/csv-parser.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TransactionEntity]), AccountsModule],
   controllers: [TransactionController],
-  providers: [TransactionService, CsvParserService, StatementSavedListener],
+  providers: [
+    TransactionService,
+    CsvParserService,
+    CsvParserFactory,
+    StatementSavedListener,
+  ],
 })
 export class TransactionsModule {}
