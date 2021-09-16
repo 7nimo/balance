6d016915be0b6a1eb084ceba0eb546e5a14a62b8
Stanisław Łagodzic <stanislaw.lagodzic@iterative.pl>
2021-09-16 18:45:36 +0200

    add fetch wrapper

---
+++ frontend/src/utils/http.util.ts
@@ -0,0 +1,61 @@
+interface HttpResponse<T> extends Response {
+  data?: T;
+}
+
+export async function http<T>(
+  request: RequestInfo
+): Promise<HttpResponse<T>> {
+  const response: HttpResponse<T> = await fetch(
+    request
+  );
+
+  try {
+    // may error if there is no body
+    response.data = await response.json();
+  } catch (error) { }
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response;
+}
+
+export async function get<T>(
+  path: string,
+  args: RequestInit = {
+    method: "get",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+): Promise<HttpResponse<T>> {
+  return await http<T>(new Request(path, args));
+};
+
+export async function post<T>(
+  path: string,
+  body: any,
+  args: RequestInit = {
+    method: "post",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }
+): Promise<HttpResponse<T>> {
+  return await http<T>(new Request(path, args));
+};
+
+export async function put<T>(
+  path: string,
+  body: any,
+  args: RequestInit = {
+    method: "put",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }
+): Promise<HttpResponse<T>> {
+  return await http<T>(new Request(path, args));
+};
