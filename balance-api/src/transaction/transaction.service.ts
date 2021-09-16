129d74ad8085424e741f9f306ddf750d925d7ec0
Stanisław Łagodzic <stanislaw.lagodzic@iterative.pl>
2021-09-16 16:46:54 +0200

    remove unused code

--- balance-api/src/transaction/transaction.service.ts
+++ balance-api/src/transaction/transaction.service.ts
@@ -39,21 +39,6 @@ export class TransactionService {
     } catch (error) {
       throw new InternalServerErrorException(error);
     }
-
-    // await queryRunner.connect();
-    // await queryRunner.startTransaction();
-    // try {
-    //   await queryRunner.manager.save(transactions[0]);
-    //   await queryRunner.manager.save(transactions[1]);
-
-    //   await queryRunner.commitTransaction();
-    // } catch (err) {
-    //   // since we have errors lets rollback the changes we made
-    //   await queryRunner.rollbackTransaction();
-    // } finally {
-    //   // you need to release a queryRunner which was manually instantiated
-    //   await queryRunner.release();
-    // }
   }
 
   async findAll(userId: string, accountId: string): Promise<TransactionsRO> {
