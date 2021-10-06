09783514480e0c96a6a9546199b81b9c0a2c05c1
elusion <stan@lagodzic.com>
2021-10-07 01:47:23 +0200

    remove password from user response object

--- balance-api/src/auth/strategies/local.strategy.ts
+++ balance-api/src/auth/strategies/local.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 import { UserService } from 'src/user/user.service';
-import { UserEntity } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -17,15 +16,16 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(email: string, password: string): Promise<UserEntity> {
-    const user = await this.userService.getUserWithPwd(email);
+  async validate(email: string, password: string) {
+    const { password: hashedPassword, ...user } =
+      await this.userService.getUserWithPwd(email);
 
     if (!user) {
       throw new UnauthorizedException('Incorrect username or password');
     }
 
     const isEqual = await this.authService.verifyPassword(
-      user.password,
+      hashedPassword,
       password,
     );
 
