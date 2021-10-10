c8dce0db0654e3d0a152304aa0164b83af7ae886
elusion <stan@lagodzic.com>
2021-10-10 21:49:21 +0200

    add some basic components for testing purposes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,20 @@
 import React from 'react';
 import './App.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 
-import { useForm } from 'react-hook-form';
-import { AuthProvider } from './api/auth';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
-type FormValues = {
-  email: string;
-  password: string;
-};
+import { useAuth } from './lib/auth';
+import { UserInfo } from './components/UserInfo';
+import { Login } from './components/Login';
+import { Button } from './components/Button';
 
 const App: React.FC = () => {
-  const { register, handleSubmit } = useForm<FormValues>();
-  const onSubmit = handleSubmit((data) => console.log(data));
-
+  const { user } = useAuth();
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <main className="App">
-          <form onSubmit={onSubmit}>
-            <input type="email" {...register('email')} />
-            <input type="password" {...register('password')} />
-
-            <input type="submit" />
-          </form>
-        </main>
-      </AuthProvider>
-
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <main>
+      {user ? <UserInfo /> : <Login />}
+      <div className="box">
+        <Button />
+      </div>
+    </main>
   );
 };
 
---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-nested-ternary */
+import { useQuery } from 'react-query';
+import { fetchAccount } from '../api/account';
+import { getNewRefreshToken } from '../api/auth';
+
+export function Button(): JSX.Element {
+  const id = 'c70555fc-d994-44fb-ac91-4cfbf72070f5';
+  const { isLoading, isFetching, data, refetch } = useQuery(
+    ['account', id],
+    () => fetchAccount(id),
+    { enabled: false }
+  );
+  const refreshToken = () => getNewRefreshToken();
+
+  return (
+    <>
+      <button type="button" onClick={() => refetch()}>
+        Fetch account
+      </button>
+
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <>
+          <div>{JSON.stringify(data)}</div>
+
+          <div>{isFetching ? 'Fetching...' : null}</div>
+        </>
+      )}
+      <button type="button" onClick={refreshToken}>
+        Refresh Token
+      </button>
+    </>
+  );
+}
