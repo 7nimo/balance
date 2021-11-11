5a49ed598c2ceb270fa2335f91f6e85f8b253a3d
elusion <stan@lagodzic.com>
2021-11-11 17:14:48 +0100

    add theming

--- balance-api/src/user/user.controller.spec.ts
+++ balance-api/src/user/user.controller.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserRO } from './user.interface';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should return a created user', async () => {
+      const user: UserRO = {
+        username: 'Joe',
+        email: 'joes@email.com',
+        password: 'password',
+      };
+
+      jest.spyOn(userService, 'create').mockImplementation(() => {
+        user;
+      });
+
+      expect(await userController.create(user)).toBe(user);
+    });
   });
 });
