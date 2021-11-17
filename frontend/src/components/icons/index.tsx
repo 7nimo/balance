c9d8bcd4127f8cd270034f9f88256961ef8f4bd9
elusion <stan@lagodzic.com>
2021-11-17 12:21:14 +0100

    add additional icons

--- frontend/src/components/icons/index.tsx
+++ frontend/src/components/icons/index.tsx
@@ -8,3 +8,8 @@ export { default as Rocket } from './Rocket';
 export { default as Settings } from './Settings';
 export { default as VisibilityHide } from './VisibilityHide';
 export { default as VisibilityShow } from './VisibilityShow';
+export { default as ChevronDown } from './ChevronDown';
+export { default as ChevronLeft } from './ChevronLeft';
+export { default as ChevronRight } from './ChevronRight';
+export { default as ChevronUp } from './ChevronUp';
+export { default as Btc } from './Btc';
