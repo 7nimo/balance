d4f08a5fe9607923cae1d2f18d95c991b09c9cee
elusion <stan@lagodzic.com>
2021-12-14 14:46:31 +0100

    update toggle hook

--- frontend/src/hooks/useToggle.ts
+++ frontend/src/hooks/useToggle.ts
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { useState } from 'react';
 
-export const useToggle = (initialValue: boolean) => {
-  const [value, setValue] = useState(initialValue);
-  const toggleValue = () => setValue(!value);
-  return [value, toggleValue] as const;
+export const useToggle = (initialValue = false): [boolean, () => void] => {
+  const [state, setValue] = useState(initialValue);
+  const toggle = (): void => setValue(!state);
+  return [state, toggle];
 };
