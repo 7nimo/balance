0e354cd71d62330bb64d254e0d03852621c4b119
elusion <stan@lagodzic.com>
2021-12-18 19:35:05 +0100

    add tooltip and hover indicator

--- frontend/src/modules/charts/components/LineChart/LineChart.tsx
+++ frontend/src/modules/charts/components/LineChart/LineChart.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useDimensions } from 'hooks/useDimensions';
-import { FC, useEffect, useRef } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 
 import { DataPoint } from '@types';
@@ -11,85 +11,80 @@ type Props = {
   assetData: DataPoint[];
 };
 
+type Point = {
+  x: number;
+  y: number;
+};
+
 export const LineChart: FC<Props> = ({ assetData }) => {
   const { observe, width, height } = useDimensions<HTMLDivElement>();
   const viewBox = `0 0 ${Math.floor(width)} ${Math.floor(height)}`;
-  const d3Container = useRef(null);
+  const [tooltipPosition, setTooltipPosition] = useState<Point>({ x: 0, y: 0 });
+  const [date, setDate] = useState<string>();
+  const [value, setValue] = useState<number>();
+  const shouldUpdate = useRef(true);
+  const d3Container = useRef<SVGSVGElement>(null);
 
+  // !¬ Data
+
+  const data: d3.InternMap<string, DataPoint[]> = d3.group(assetData, (d: DataPoint) => d.date);
+
+  // !¬ Accesors
+  const dateParser = d3.timeParse('%Y-%m-%d');
+
+  const x = ([x]: [string, DataPoint[]]): Date => dateParser(x)!;
+  const y = ([, y]: [string, DataPoint[]]): number => y[0].value;
+
+  // Compute values.
+  const X = d3.map(data, x);
+  const Y = d3.map(data, y);
+  // const O = d3.map(data, (d) => d);
+  // const I = d3.map(data, (_, i) => i);
+
+  // Compute which data points are considered defined.
+  const defined = (d: unknown, i: number): boolean => X[i] instanceof Date && !Number.isNaN(Y[i]);
+  const D = d3.map(data, defined);
+
+  // !¬ Scales
+  const xExtent = d3.extent(data, x) as [Date, Date];
+  const xScale = d3.scaleTime().domain(xExtent).range([0, width]);
+
+  const yExtent = d3.extent(data, y) as [number, number];
+  const yScale = d3.scaleLinear().domain(yExtent).range([height, 0]);
+
+  // !¬ Line generator
+  const lineGenerator = d3
+    .line()
+    .defined(([i, _]) => D[i] as boolean)
+    .x(([i]) => xScale(X[i]!))
+    .y(([i]) => yScale(Y[i]));
+  // .curve(d3.curveBasis)
+
+  // !!¬ Render
   useEffect(() => {
-    if (assetData.length && d3Container.current) {
+    if (assetData.length && d3Container.current && shouldUpdate.current === true) {
       const svg = d3.select(d3Container.current);
-
-      // !¬ Data
-
-      const data: d3.InternMap<string, DataPoint[]> = d3.group(assetData, (d: DataPoint) => d.date);
-
-      // !¬ Accesors
-      const dateParser = d3.timeParse('%Y-%m-%d');
-
-      const x = ([x]: [string, DataPoint[]]): Date => dateParser(x)!; // assume date always present
-      const y = ([, y]: [string, DataPoint[]]): number => y[0].value;
-
-      // Compute values.
-      const X = d3.map(data, x);
-      const Y = d3.map(data, y);
-      const O = d3.map(data, (d) => d);
-      const I = d3.map(data, (_, i) => i);
-
-      // Compute which data points are considered defined.
-      const defined = (d: unknown, i: number): boolean =>
-        X[i] instanceof Date && !Number.isNaN(Y[i]);
-      const D = d3.map(data, defined);
-
-      // !¬ Scales
-      const xExtent = d3.extent(data, x) as [Date, Date];
-      const xScale = d3.scaleTime().domain(xExtent).range([0, width]);
-
-      const yExtent = d3.extent(data, y) as [number, number];
-      const yScale = d3.scaleLinear().domain(yExtent).range([height, 0]);
-
-      // !¬ Line generator
-      const lineGenerator = d3
-        .line()
-        .defined(([i, _]) => D[i] as boolean)
-        // .curve(d3.curveBasis)
-        .x(([i]) => xScale(X[i]!))
-        .y(([i]) => yScale(Y[i]));
+      shouldUpdate.current = false;
 
       // !¬ Path generator
       svg
         .append('g')
         .append('path')
         .attr('fill', 'none')
-        .attr('stroke', 'var(--green-light)')
+        .attr('stroke', 'var(--orange)')
         .attr('stroke-width', 2)
         .attr('d', lineGenerator(d3.map(data, (_, i) => [i, i])));
 
-      // !¬ X Axis
-      const xAxisGenerator = d3.axisBottom(xScale);
-      const xAxis = svg.append('g').call(xAxisGenerator);
-
-      // !¬ Y Axis
-      const yAxisGenerator = d3.axisLeft(yScale);
-      const yAxis = svg.append('g').call(yAxisGenerator);
-
       // !¬ Tooltip
-      const tooltip = svg
-        .append('g')
-        .append('rect')
-        .attr('fill', 'transparent')
-        .style('pointer-events', 'none')
-        .attr('width', width)
-        .attr('height', height);
 
-      const onMouseMove = (event: any) => {
-        console.log(event);
-      };
-      const onMouseLeave = (event: any) => {
-        console.log(event);
-      };
-
-      tooltip.on('onMouseOver', onMouseMove);
+      svg
+        .append('circle')
+        .attr('id', 'circle')
+        .attr('r', 4)
+        .attr('stroke', 'white')
+        .attr('fill', '#orange')
+        .attr('stroke-width', 2)
+        .style('opacity', 0);
 
       // const xAxisLine = svg
       //   .append('g')
@@ -99,29 +94,76 @@ export const LineChart: FC<Props> = ({ assetData }) => {
       //   .attr('width', '.5px')
       //   .attr('height', height);
 
-      // const getDistanceFromHoveredDate = (d) => Math.abs(Number(xAccessor(d)!) - hoveredDate);
+      // // !¬ X Axis
+      // const xAxisGenerator = d3.axisBottom(xScale);
+      // const xAxis = svg.append('g').call(xAxisGenerator);
 
-      // const closestIndex = d3.scan(
-      //   data,
-      //   (a: any, b: any) => getDistanceFromHoveredDate(a) - getDistanceFromHoveredDate(b)
-      // );
+      // // !¬ Y Axis
+      // const yAxisGenerator = d3.axisLeft(yScale);
+      // const yAxis = svg.append('g').call(yAxisGenerator);
     }
-  }, [assetData, height, width]);
+  }, [assetData, data, lineGenerator]);
+
+  const onMouseEnter = (): void => {
+    d3.select('#tooltip').style('opacity', 1);
+    d3.select('#circle').style('opacity', 1);
+  };
+
+  const onMouseMove = (event: React.SyntheticEvent): void => {
+    const hoveredDate = xScale.invert(d3.pointer(event)[0]);
+    // const i = d3.bisectCenter(X, xScale.invert(d3.pointer(event)[0]));
+
+    const closestIndex = d3.leastIndex(
+      data,
+      (a, b) =>
+        Math.abs((x(a) as any) - (hoveredDate as any)) -
+        Math.abs((x(b) as any) - (hoveredDate as any))
+    );
+
+    const formatDate = d3.timeFormat('%Y-%m-%d');
+
+    const xValue = X[closestIndex!];
+    const yValue = Y[closestIndex!];
+
+    const date = formatDate(xValue);
+
+    const xPos: number = xScale(xValue);
+    const yPos: number = yScale(yValue);
+    setTooltipPosition({ x: xPos - 75, y: yPos - 80 });
+    d3.select('#circle').attr('cx', xPos).attr('cy', yPos);
+
+    setDate(date);
+    setValue(yValue);
+
+    // data.get(date);
+  };
+
+  const onMouseLeave = (): void => {
+    d3.select('#tooltip').style('opacity', 0);
+    d3.select('#circle').style('opacity', 0);
+  };
 
   return (
     <div className={s.chartWrapper} ref={observe}>
-      <div id="tooltip" className="tooltip">
-        <div className="tooltip-value">
-          <span id="value" />
-        </div>
-        <div className="tooltip-date">
-          <span id="date" />
+      <div
+        id="tooltip"
+        className={s.tooltip}
+        style={{ left: tooltipPosition.x ?? 0, top: tooltipPosition.y ?? 0 }}
+      >
+        <div className={s.tooltipValue}>
+          <span className={s.value}>{`£${value}`}</span>
+          <span className={s.date}>{date}</span>
         </div>
       </div>
 
-      <svg style={{ overflow: 'visible' }} viewBox={viewBox} ref={d3Container}>
-        {/* <rect width={Math.floor(width)} height={Math.floor(height)} fill="var(--card-bg)" /> */}
-      </svg>
+      <svg
+        style={{ overflow: 'visible' }}
+        viewBox={viewBox}
+        ref={d3Container}
+        onMouseEnter={d3Container.current ? onMouseEnter : undefined}
+        onMouseMove={d3Container.current ? onMouseMove : undefined}
+        onMouseLeave={d3Container.current ? onMouseLeave : undefined}
+      />
     </div>
   );
 };
