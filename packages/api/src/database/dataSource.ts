122e4b2f082a47325c012e828e5b2a3e8d8f5d51
Stan Lagodzic <stan@lagodzic.com>
2022-05-05 14:24:08 +0000

    update typeorm & db module connection using DataSource

---
+++ packages/api/src/database/dataSource.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ['dist/**/entities/*.entity{.ts,.js}'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  synchronize: true,
+});
