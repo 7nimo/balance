6dff3518de10c0e9f0cc77293b1a6361b9cd613f
Stan Lagodzic <stan@lagodzic.com>
2022-05-05 16:48:56 +0000

    clean up structure; part1

--- packages/ui/src/utils/storage.ts
+++
@@ -1,6 +0,0 @@
-import { Theme } from '@types';
-
-export const storage = {
-  getTheme: () => window.localStorage.getItem('theme'),
-  setTheme: (theme: Theme) => window.localStorage.setItem('theme', theme)
-};
