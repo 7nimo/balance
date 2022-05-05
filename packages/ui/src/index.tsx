a1b0207c6254b5b17c7ee0ec4cb3753f520a7c3b
Stan Lagodzic <stan@lagodzic.com>
2022-05-05 17:26:11 +0000

    upgrade to react 18

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,11 +1,11 @@
-import './index.scss';
+import './global.scss';
 
 import App from 'components/_App/App';
 import ReactQueryProvider from 'core/lib/react-query';
 import { themeActions } from 'core/store/services/theme/slice';
 import { store } from 'core/store/store';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -13,13 +13,15 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   store.dispatch(themeActions.changeColorScheme('dark'));
 }
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('app');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
+root.render(
+  <StrictMode>
     <ReactQueryProvider>
       <App />
     </ReactQueryProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
