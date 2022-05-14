586dc088fe0f1db5729b3dcc524742219ff60827
Stan Lagodzic <stan@lagodzic.com>
2022-05-14 19:40:35 +0000

    various

---
+++ packages/ui/src/components/Modal/Dialog.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Dialog = styled.div`
+  outline: currentcolor none medium;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+`;
