920b28283e9ff18d5b989b02de9b9ddaa3d6a423
Stan Lagodzic <stan@lagodzic.com>
2022-05-20 13:21:28 +0000

    add modal component

--- packages/ui/src/pages/AccountsPage/AccountsPage.tsx
+++ packages/ui/src/pages/AccountsPage/AccountsPage.tsx
@@ -2,21 +2,22 @@ import { AccountEntity } from '@types';
 import { IconButton } from 'components/buttons/IconButton/IconButton';
 import SvgAdd from 'components/icons/actions/Add';
 import ColorLine from 'components/misc/ColorLine';
+import Modal from 'components/Modal/Modal';
 import { RelativeElements } from 'components/RelativeElements/RelativeElements';
 import { Toolbox } from 'components/RelativeElements/Toolbox';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useMatch } from 'react-location';
 
 import { ReactComponent as Logo } from '../../assets/svg/banks/lloyds.svg';
-import { Wrapper } from '../../components/_Layout/Root/Wrapper';
 import s from './AccountsPage.module.scss';
 import { Wrapper } from './Wrapper';
 
 function AccountsPage (): React.ReactElement {
   const { data: { accounts } } = useMatch();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleAddAccount = () => {
-    // open modal
+    setIsOpen(true);
   };
 
   const renderAccounts = (accounts as AccountEntity[]).map((account) => {
@@ -40,21 +41,25 @@ function AccountsPage (): React.ReactElement {
   });
 
   const renderActions = (
-    <div>
-      <IconButton
-        icon={<SvgAdd />}
-        onClick={handleAddAccount}
-      />
-    </div>
+    <IconButton
+      icon={<SvgAdd />}
+      onClick={handleAddAccount}
+    />
   );
 
   return (
     <Wrapper>
-      {renderAccounts}
       <RelativeElements>
         <Toolbox>
           {renderActions}
         </Toolbox>
+        <Modal
+          handleClose={() => setIsOpen(false)}
+          isOpen={isOpen}
+          title='Add Account'
+        >
+          This is Modal Content!
+        </Modal>
       </RelativeElements>
       {renderAccounts}
     </Wrapper>
