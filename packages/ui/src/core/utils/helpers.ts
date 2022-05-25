a88bc7f85df75a346291a930d0c69de12b639dc7
nimo <nimo@elusion.io>
2022-05-25 11:10:43 +0000

    add removeEmptyFields utility function

--- packages/ui/src/core/utils/helpers.ts
+++ packages/ui/src/core/utils/helpers.ts
@@ -19,3 +19,11 @@ export function debounce (fn: any, wait: number) {
     timeout = setTimeout(callback, wait);
   };
 }
+
+export function removeEmptyFields (data: any) {
+  Object.keys(data).forEach((key) => {
+    if (data[key] === '' || data[key] == null) {
+      delete data[key];
+    }
+  });
+}
