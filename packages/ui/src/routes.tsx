86bfca86014d5607fcc5d918822780ab21bf69be
nimo <nimo@elusion.io>
2022-05-25 13:59:26 +0000

    update routes with types and data loaders

--- packages/ui/src/routes.tsx
+++ packages/ui/src/routes.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable sort-keys */
+import { AccountEntity, Transaction, UserEntity } from '@types';
 import Root from 'components/_Layout/Root/Root';
 import SignInForm from 'components/forms/SignInForm/SignInForm';
 import SignUpForm from 'components/forms/SignUpForm/SignUpForm';
@@ -17,8 +18,13 @@ import CryptoPage from './pages/CryptoPage/CryptoPage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import SettingsPage from './pages/SettingsPage/SettingsPage';
 
-type LocationGenerics = MakeGenerics<{
-  Params: { accountId: string };
+export type LocationGenerics = MakeGenerics<{
+  LoaderData: {
+    account: AccountEntity;
+    accounts: AccountEntity[];
+    transactions: Transaction[];
+    user: UserEntity;
+  };
 }>;
 
 export const location = new ReactLocation<LocationGenerics>();
@@ -47,24 +53,32 @@ export const routes: Route<LocationGenerics>[] = [
           {
             path: '/',
             element: <AccountsPage />,
-            loader: () =>
-              queryClient.getQueryData('accounts') ??
-              queryClient.fetchQuery('accounts', () => fetchAccounts())
-          },
-          {
-            path: ':accountId/settings',
-            element: <AccountSettings />
+            loader: async () => ({
+              accounts: queryClient.getQueryData('accounts') ??
+              await queryClient.fetchQuery('accounts', fetchAccounts)
+            })
           },
           {
             path: ':accountId',
             element: <AccountContainer />,
-            loader: ({ params: { accountId } }) =>
-              queryClient.getQueryData(['transactions', accountId]) ??
-              queryClient.fetchQuery(['transactions', accountId], () =>
-                fetchTransactionsByAccountId(accountId)
-              )
-            // todo: error element
-            // element: <AccountOverview />
+            loader: async ({ params: { accountId } }, { parentMatch }) => ({
+              account: await parentMatch?.loaderPromise?.then(({ accounts }) =>
+                accounts?.find((account) => account.id === accountId)
+              ),
+              transactions: queryClient.getQueryData(['transactions', accountId]) ??
+                await queryClient.fetchQuery(['transactions', accountId],
+                () => fetchTransactionsByAccountId(accountId))
+            }),
+            children: [
+              // {
+              //   path: '/',
+              //   element: <AccountOverview />
+              // },
+              {
+                path: 'settings',
+                element: <AccountSettings />
+              }
+            ]
           }
         ]
       },
