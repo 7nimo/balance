2a2d8014c02edcc49541e7d368b513edd1a7b754
nimo <nimo@elusion.io>
2022-05-25 11:14:40 +0000

    add layout components

---
+++ packages/ui/src/components/_Layout/Layout.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1 1 auto;
+  height: 100%;
+  width: 100%;
+  padding: 0 24px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 0 1 auto;
+  height: 100%;
+  width: 100%;
+  padding: 24px 0;
+  max-width: 1200px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
